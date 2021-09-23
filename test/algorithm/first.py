8731beb8731ae5d27f9a67bec707f795505a7092
Max <bigin.maks@gmail.com>
2021-09-23 23:28:34 +0300

    finish

--- test/algorithm/first.py
+++ test/algorithm/first.py
@@ -3,14 +3,23 @@ string = list(input.replace(' ', ''))
 
 space = input.count(' ')
 
+list = []
+upper = []
 key_upper = []
 key_lower = []
-str_list = []
 
-for i in string:
-    index = string.index(i)
-    str_list.append({index: i})
-    key_upper.append(index) if i.isupper() else key_lower.append(index)
+for char in string:
+    index = string.index(char)
+    key_upper.append(index) if char.isupper() else key_lower.append(index)
+    list.append(char)
     string[index] = None
 
-print(len(key_upper)*2 + len(key_lower) + space)
+for i in key_upper:
+    try:
+        if key_upper[key_upper.index(i)+1] == i+1:
+            upper.append(i)
+    except IndexError:
+        if key_upper[key_upper.index(i)-1] == i-1:
+            upper.append(i)
+
+print((2 + len(upper) if len(upper) else len(key_upper)*2) + len(key_lower) + space)
