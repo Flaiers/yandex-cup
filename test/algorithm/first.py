78898a9304123d6f3f9096f391bd76b4e84b23a0
Max <bigin.maks@gmail.com>
2021-09-24 00:01:04 +0300

    fix, todo splitting a list

--- test/algorithm/first.py
+++ test/algorithm/first.py
@@ -3,23 +3,28 @@ string = list(input.replace(' ', ''))
 
 space = input.count(' ')
 
-list = []
 upper = []
+next_upper = []
 key_upper = []
 key_lower = []
 
 for char in string:
     index = string.index(char)
     key_upper.append(index) if char.isupper() else key_lower.append(index)
-    list.append(char)
     string[index] = None
 
 for i in key_upper:
     try:
         if key_upper[key_upper.index(i)+1] == i+1:
             upper.append(i)
+        elif key_upper[key_upper.index(i)-1] == i-1:
+            upper.append(i)
+        else:
+            next_upper.append(i)
     except IndexError:
         if key_upper[key_upper.index(i)-1] == i-1:
             upper.append(i)
+        else:
+            next_upper.append(i)
 
-print((2 + len(upper) if len(upper) else len(key_upper)*2) + len(key_lower) + space)
+print((2 + len(upper) + 2 + len(next_upper) if len(upper) else len(next_upper)*2) + len(key_lower) + space)
