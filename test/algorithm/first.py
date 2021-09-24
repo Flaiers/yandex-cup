987767bf25d2d5cbc6d9dd1ad34f1b5981f71cf5
Max <bigin.maks@gmail.com>
2021-09-24 13:29:11 +0300

    count caps uppers

--- test/algorithm/first.py
+++ test/algorithm/first.py
@@ -1,6 +1,7 @@
 input = input()
 string = list(input.replace(' ', ''))
 
+count = 0
 space = input.count(' ')
 
 caps_upper = []
@@ -14,28 +15,34 @@ for char in string:
     uppers.append(index) if char.isupper() else letters.append(index)
     string[index] = None
 
-for i in uppers:
-    try:
-        if uppers[uppers.index(i)+3] == i+3:
-            after_upper.append(i)
+if len(uppers):
+    for i in uppers:
+        try:
+            if uppers[uppers.index(i)+1] == i+1:
+                after_upper.append(i)
+            elif uppers[uppers.index(i)-1] == i-1:
+                after_upper.append(i)
+            else:
+                shift_upper.append(i)
+        except IndexError:
+            if uppers[uppers.index(i)-1] == i-1:
+                after_upper.append(i)
+            else:
+                shift_upper.append(i)
+
+    row = []
+    row.append(after_upper[0])
+
+    for i in range(len(after_upper) - 1):
+        if after_upper[i] == after_upper[i + 1] - 1:
+            row.append(after_upper[i + 1])
         else:
-            shift_upper.append(i)
-    except IndexError:
-        if uppers[uppers.index(i)-1] == i-1:
-            after_upper.append(i)
-        else:
-            shift_upper.append(i)
+            caps_upper.append(row)
+            row = []
+            row.append(after_upper[i + 1])
+    caps_upper.append(row)
 
-row = []
-row.append(after_upper[0])
+    for i in caps_upper:
+        count += 2 + len(i) + 2
 
-for i in range(len(after_upper) - 1):
-    if after_upper[i] == after_upper[i + 1] - 1:
-        row.append(after_upper[i + 1])
-    else:
-        caps_upper.append(row)
-        row = []
-        row.append(after_upper[i + 1])
-caps_upper.append(row)
-
-# print((2 + len(after_upper) + 2 + len(shift_upper) if len(after_upper) else len(shift_upper)*2) + len(letters) + space)
+print(space + count + len(shift_upper) + len(letters))
