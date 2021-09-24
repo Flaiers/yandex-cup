3fdd9c57908b968201fd388af844a230313c9933
Max <bigin.maks@gmail.com>
2021-09-24 15:24:02 +0300

    edit loop logic

--- test/algorithm/first.py
+++ test/algorithm/first.py
@@ -16,33 +16,43 @@ for char in string:
     string[index] = None
 
 if len(uppers):
-    for i in uppers:
+    for el in uppers:
+        i = uppers.index(el)
         try:
-            if uppers[uppers.index(i)+1] == i+1:
-                after_upper.append(i)
-            elif uppers[uppers.index(i)-1] == i-1:
-                after_upper.append(i)
+            if uppers[i] - uppers[i - 3] == 3 or uppers[i + 3] - uppers[i] == 3:
+                if el not in after_upper:
+                    after_upper.append(el)
+            elif uppers[i + 1] - uppers[i] != 1:
+                if el not in after_upper:
+                    shift_upper.append(el)
+            elif uppers[i] - uppers[i - 2] == 2:
+                if el not in after_upper:
+                    after_upper.append(el)
             else:
-                shift_upper.append(i)
+                if el not in after_upper:
+                    shift_upper.append(el)
         except IndexError:
-            if uppers[uppers.index(i)-1] == i-1:
-                after_upper.append(i)
+            if uppers[i] - uppers[i - 3] == 3:
+                if el not in after_upper:
+                    after_upper.append(el)
             else:
-                shift_upper.append(i)
+                if el not in after_upper:
+                    shift_upper.append(el)
 
-    row = []
-    row.append(after_upper[0])
+    if len(after_upper):
+        row = []
+        row.append(after_upper[0])
 
-    for i in range(len(after_upper) - 1):
-        if after_upper[i] == after_upper[i + 1] - 1:
-            row.append(after_upper[i + 1])
-        else:
-            caps_upper.append(row)
-            row = []
-            row.append(after_upper[i + 1])
-    caps_upper.append(row)
+        for i in range(len(after_upper) - 1):
+            if after_upper[i] == after_upper[i + 1] - 1:
+                row.append(after_upper[i + 1])
+            else:
+                caps_upper.append(row)
+                row = []
+                row.append(after_upper[i + 1])
+        caps_upper.append(row)
 
-    for i in caps_upper:
-        count += 2 + len(i) + 2
+        for i in caps_upper:
+            count += 2 + len(i) + 2
 
-print(space + count + len(shift_upper) + len(letters))
+print(space + count + len(shift_upper)*2 + len(letters))
